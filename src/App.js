6d800c71d957bba4ccf49ecba1ed3edd42051a46
vedatkokmen <vedatkoekmen@gmail.com>
2021-07-01 20:53:34 +0300

    Complete InstaLoginAlt component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import InstaLogin from "./components/InstaLogin";
+//import InstaLogin from "./components/InstaLogin";
+import InstaLoginAlt from "./components/InstaLoginAlt";
 //import InstaStory from "./components/InstaStory";
 
 const App = () => {
   return (
     <>
-      <InstaLogin />
+      <InstaLoginAlt />
     </>
   );
 };
